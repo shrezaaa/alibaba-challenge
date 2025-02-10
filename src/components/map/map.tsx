6b1873b53870ba88e19b34a60c31716de1b85264
Reza Shakeri <53859479+shrezaaa@users.noreply.github.com>
2025-02-10 19:52:55 +0330

    featL update map to support multi marker

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -2,57 +2,64 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
 import "leaflet-defaulticon-compatibility";
 
-import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
-import { LatLngExpression, divIcon } from "leaflet";
-import { useState } from "react";
-import React from "react";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { LatLngExpression, divIcon, LatLngBounds } from "leaflet";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationPin } from "@fortawesome/free-solid-svg-icons";
 import { renderToString } from "react-dom/server";
+import { Hotel } from "../../types/hotel.model";
+
 interface MapProps {
-  position: LatLngExpression;
-  center: LatLngExpression;
-  showMarker: boolean;
-  zoom?: number;
+  hotels: Hotel[];
   height?: string;
   width?: string;
 }
 
-const defaults = { zoom: 14 };
-
-const locationIconMarker = () => {
-  const iconHtml = renderToString(  
-    React.createElement(FontAwesomeIcon, {
-      icon: faLocationPin,
-      style: { fontSize: "30px", color: "green" },
-    })
+const locationIconMarker = (hotelName: string) => {
+  const iconHtml = renderToString(
+    <div className="flex flex-col items-center">
+      <FontAwesomeIcon
+        icon={faLocationPin}
+        className="text-green-800 text-3xl opacity-95"
+      />
+      <div className="text-xs font-semibold text-center mt-1 text-white bg-green-800 py-1 px-2 rounded-lg opacity-95">
+        {hotelName}
+      </div>
+    </div>
   );
 
   return divIcon({
     html: iconHtml,
     className: "custom-marker-icon",
-    iconSize: [30, 30],
-    iconAnchor: [12, 30],
+    iconSize: [30, 42],
+    iconAnchor: [15, 42],
   });
 };
 
-export default function Map(props: MapProps) {
-  const {
-    zoom = defaults.zoom,
-    position,
-    center,
-    showMarker,
-    width = "100%",
-    height = "400px",
-  } = props;
-  const [markerPosition, setMarkerPosition] =
-    useState<LatLngExpression>(position);
+// Component to handle map bounds and zoom
+const MapBounds = ({ hotels }: { hotels: Hotel[] }) => {
+  const map = useMap();
 
+  useEffect(() => {
+    if (hotels.length > 0) {
+      // Create a LatLngBounds object to fit all markers
+      const bounds = new LatLngBounds(
+        hotels.map((hotel) => [hotel.location.lat, hotel.location.long])
+      );
+
+      // Fit the map to the bounds
+      map.fitBounds(bounds, { padding: [50, 50] }); // Add padding for better visibility
+    }
+  }, [hotels, map]);
+
+  return null;
+};
+
+const Map = ({ hotels, height = "400px", width = "100%" }: MapProps) => {
   return (
     <MapContainer
       preferCanvas={true}
-      center={center}
-      zoom={zoom}
       scrollWheelZoom={true}
       style={{ height, width }}
     >
@@ -60,11 +67,16 @@ export default function Map(props: MapProps) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {showMarker && (
-        <>
-          <Marker position={markerPosition} icon={locationIconMarker()} />
-        </>
-      )}
+      {hotels.map((hotel) => (
+        <Marker
+          key={hotel.id}
+          position={[hotel.location.lat, hotel.location.long]}
+          icon={locationIconMarker(hotel.name)} // Pass hotel name
+        />
+      ))}
+      <MapBounds hotels={hotels} /> {/* Add MapBounds component */}
     </MapContainer>
   );
-}
+};
+
+export default Map;
