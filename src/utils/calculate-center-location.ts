f3bf5fbdece23f8216e8d76d38f914610cf1262a
Reza Shakeri <53859479+shrezaaa@users.noreply.github.com>
2025-02-10 16:53:18 +0330

    feat: add centralize logic for lat long

---
+++ src/utils/calculate-center-location.ts
@@ -0,0 +1,22 @@
+type LatLng = { lat: number; long: number };
+
+const calculateCenter = (points: LatLng[]): LatLng => {
+  if (points.length === 0) {
+    throw new Error("No points provided to calculate the center.");
+  }
+
+  const total = points.reduce(
+    (acc, point) => {
+      return {
+        lat: acc.lat + point.lat,
+        long: acc.long + point.long,
+      };
+    },
+    { lat: 0, long: 0 }
+  );
+
+  return {
+    lat: total.lat / points.length,
+    long: total.long / points.length,
+  };
+};
