0c166dfaadae4480e9ea148b430bf1880af9584d
Reza Shakeri <53859479+shrezaaa@users.noreply.github.com>
2025-02-11 01:36:24 +0330

    feat: adjust navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,21 +2,9 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="sticky top-0 bg-white h-14 w-full z-50 border-b">
-      <div className="flex h-full justify-end items-center">
+    <nav className="sticky top-0 bg-white h-14 w-full z-50 border-b shadow-sm">
+      <div className="flex h-full items-center">
         <div className="ps-2">Alibaba Challenge</div>
-
-        <div className="mr-auto"></div>
-
-        {/* <div className="py-2 pr-2 ml-auto my-2 text-black rounded-full hover:text-yellow-600 cursor-pointer">
-          <button>
-            <span className="material-icons">notifications</span>
-          </button>
-        </div> */}
-
-        {/* <div className="py-2 mx-2 rounded-sm text-black cursor-pointer flex flex-col items-end">
-          <span>Admin</span>
-        </div> */}
       </div>
     </nav>
   );
